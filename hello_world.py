ed16f9c9ffc158bba9b5daa64f3e029e18a3dc2b
divyanshu sharma <divyanshusharma7860@gmail.com>
2024-08-01 11:43:05 +0530

    Adding new function

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,8 @@
 def hello_world():
     return "Hello, World!"
 
+def bye_world():
+    return  "Bye,World!"
+
 if __name__ == "__main__":
     print(hello_world())
--- test_hello_world.py
+++ test_hello_world.py
@@ -1,9 +1,14 @@
 import unittest
-from hello_world import hello_world
+from hello_world import hello_world,bye_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, World!")
+
+    
+class TestByeWorld(unittest.TestCase):
+    def test_bye_world(self):
+        self.assertEqual(bye_world,"Bye,World!")
 
 if __name__ == '__main__':
     unittest.main()
