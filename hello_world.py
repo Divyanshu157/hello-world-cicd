95691575535ed609534a86263a2e368eb79abc70
divyanshu sharma <divyanshusharma7860@gmail.com>
2024-08-01 16:45:17 +0530

    adding spaces

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,12 @@
 def hello_world():
-    return "Hello, World!"
+    return "Hello, orld!"
 
 def bye_world():
     return  "Bye,World!"
 
 if __name__ == "__main__":
 
-    
+
     print(hello_world())
     print(bye_world())
     
